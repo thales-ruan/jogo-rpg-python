39e388ff6955aac977f626fc9da9c925e1801473
Thales Ruan <thalesruandamassacla@gmail.com>
2023-03-25 19:42:14 -0300

    adicionando a funcao de comprar na loja

--- loja.py
+++ loja.py
@@ -12,3 +12,17 @@ class Loja:
             item = self.items[nome_do_item]
             print(f"{i+1}. {nome_do_item} - Preço: {item['preco']}")
 
+    def comprar_item(self, nome_do_item, personagem):
+        if nome_do_item in self.itens:
+            item = self.itens[nome_do_item]
+            if personagem.ouro >= item["preco"]:
+                personagem.ouro -= item["preco"]
+                tipo_do_item = nome_do_item.split()[0] 
+                if tipo_do_item == "Espada" or tipo_do_item == "Machado":
+                    personagem.ponto_de_ataque += item["dano"]
+                    personagem.arma = nome_do_item
+                print(f"{personagem.nome} comprou {nome_do_item}!")
+            else:
+                print("Você não tem ouro suficiente para comprar este item.")
+        else:
+            print("Este item não está disponível na loja.")
